6359ae8f9e7cc07d9595246b3b5e7d6796a3b862
Ben Elliot <benjiboi214@gmail.com>
2019-10-31 19:08:01 +1100

    Remove debugging logs

--- build-scripts/production/pull.sh
+++ build-scripts/production/pull.sh
@@ -9,9 +9,6 @@ export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-"NONE"}"
 export DJANGO_SECRETS_PATH="${DJANGO_SECRETS_PATH:-"./.envs/.production/.django"}"
 export POSTGRES_SECRETS_PATH="${POSTGRES_SECRETS_PATH:-"./.envs/.production/.postgres"}"
 
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-
 eval $(AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" aws ecr get-login --no-include-email)
 
 docker-compose -f production.yml pull
