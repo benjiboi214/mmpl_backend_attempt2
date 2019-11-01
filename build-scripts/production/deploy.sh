cea50e73aaedc098f1d5765395e55d6a6f60446a
Ben Elliot <benjiboi214@gmail.com>
2019-11-01 12:14:07 +1100

    Should not be NONE anymore for AWS Secret

--- build-scripts/production/deploy.sh
+++ build-scripts/production/deploy.sh
@@ -12,8 +12,8 @@ export GIT_HTTPS_URL="${GIT_HTTPS_URL:-"https://github.com/benjiboi214/mmpl_back
 export GIT_BRANCH="${GIT_BRANCH:-"feature/implement_deploy_step"}"
 export DOCKER_REGISTRY="${DOCKER_REGISTRY:-"413514076128.dkr.ecr.ap-southeast-2.amazonaws.com"}"
 export BUILD_VERSION="${BUILD_VERSION:-"0.0.1"}"
-export AWS_ECR_KEY_ID="${AWS_ECR_KEY_ID:-"NONE"}"
-export AWS_ECR_ACCESS_KEY="${AWS_ECR_ACCESS_KEY:-"NONE"}"
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-"NONE"}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-"NONE"}"
 export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-"ap-southeast-2"}"
 export DJANGO_SECRETS_PATH="${DJANGO_SECRETS_PATH:-"./.envs/.production/.django"}"
 export POSTGRES_SECRETS_PATH="${POSTGRES_SECRETS_PATH:-"./.envs/.production/.postgres"}"
@@ -26,7 +26,7 @@ ansible-playbook -v -i staging.mmpl.systemiphus.com, \
   -u $DEPLOY_USER --key-file=$SSH_KEY \
   -e "github_repo=$GIT_HTTPS_URL github_version=$GIT_BRANCH \
       docker_registry=$DOCKER_REGISTRY build_version=$BUILD_VERSION \
-      aws_ecr_key_id=$AWS_ECR_KEY_ID aws_ecr_access_key=$AWS_ECR_ACCESS_KEY \
+      aws_ecr_key_id=$AWS_ACCESS_KEY_ID aws_ecr_access_key=$AWS_SECRET_ACCESS_KEY \
       django_secrets_path=$DJANGO_SECRETS_PATH postgres_secrets_path=$POSTGRES_SECRETS_PATH \
       aws_default_region=$AWS_DEFAULT_REGION" \
   deploy.yml
