7dd5027958857c220ca61649922e9a08225c6140
Ben Elliot <benjiboi214@gmail.com>
2020-02-02 21:57:59 +1100

    Add migrations step for running the image

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
 pipenv run python manage.py runserver_plus 0.0.0.0:$DJANGO_INTERNAL_PORT_NUM
