fc5841b1202676a8de5faf7f45335864a259c437
Ben Elliot <benjiboi214@gmail.com>
2020-02-04 13:38:02 +1100

    RED: Missing flake and coverage deps

--- build_scripts/local/unit_test.sh
+++ build_scripts/local/unit_test.sh
@@ -1,4 +1,17 @@
 #/bin/bash
 
+echo "#### Running PyTest ####"
 pipenv run pytest \
   --junitxml=${PYTEST_REPORT_OUTPUT:-"./pytest.xml"}
+
+echo "#### Running Flake8 ####"
+pipenv run flake8 \
+  --format junit-xml \
+  --output-file=${FLAKE8_REPORT_OUTPUT:-"./flake8.xml"} \
+  --config=${FLAKE8_CONFIG_FILE:-"setup.cfg"} .
+
+echo "#### Running Coverage ####"
+pipenv run coverage \
+  run -m pytest
+pipenv run coverage \
+  xml -o ${COVERAGE_REPORT_OUTPUT:-"/coverage.xml"}
