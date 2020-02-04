092d59e03d6d44a141d7634aeef9091d426e0eb4
Ben Elliot <benjiboi214@gmail.com>
2020-02-04 14:58:19 +1100

    Move test config to folder

--- build_scripts/local/unit_test.sh
+++ build_scripts/local/unit_test.sh
@@ -1,21 +1,25 @@
 #/bin/bash
 
+source .env
+
 echo "#### Running PyTest ####"
-pipenv run pytest \
-  --junitxml=${PYTEST_REPORT_OUTPUT:-"./pytest.xml"}
+pipenv run pytest ./api \
+  -c $PYTEST_CONFIG_PATH \
+  --junitxml=${PYTEST_REPORT_PATH:-"./pytest.xml"}
 
 echo "#### Running Flake8 ####"
 pipenv run flake8 \
-  --config=${FLAKE8_CONFIG_FILE:-"flake8.cfg"} .
+  --config=${FLAKE8_CONFIG_PATH:-"flake8.cfg"} .
 pipenv run flake8 \
   --format junit-xml \
-  --output-file=${FLAKE8_REPORT_OUTPUT:-"./flake8.xml"} \
-  --config=${FLAKE8_CONFIG_FILE:-"flake8.cfg"} .
+  --output-file=${FLAKE8_REPORT_PATH:-"./flake8.xml"} \
+  --config=${FLAKE8_CONFIG_PATH:-"flake8.cfg"} .
 
 echo "#### Running Coverage ####"
 # pipenv run coverage \
 #   run -m pytest
 # pipenv run coverage \
 #   xml -o ${COVERAGE_REPORT_OUTPUT:-"./coverage.xml"}
-pipenv run pytest --cov=api \
-  --junitxml=${PYTEST_REPORT_OUTPUT:-"./coverage.xml"}
+pipenv run pytest --cov=api ./api \
+  -c $PYTEST_CONFIG_PATH \
+  --junitxml=${COVERAGE_REPORT_PATH:-"./coverage.xml"}
