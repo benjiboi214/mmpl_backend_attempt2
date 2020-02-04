cb825222943247be9122662c3a447187a11e8057
Ben Elliot <benjiboi214@gmail.com>
2020-02-04 16:17:36 +1100

    Start FF in headless mode

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,13 @@
 import os
 
 from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
 
 host = os.getenv("DJANGO_EXTERNAL_HOST_NAME", "")
 port = os.getenv("DJANGO_EXTERNAL_PORT_NUM", "")
+
+opts = FirefoxOptions()
+opts.add_argument("--headless")
 
 browser = webdriver.Firefox()
 browser.get(f'http://{host}:{port}')
