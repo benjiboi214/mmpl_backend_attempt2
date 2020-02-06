ff9fde58a6e56ab692c9a67c43a6f7a9c6657d2a
Ben Elliot <benjiboi214@gmail.com>
2020-02-07 08:43:57 +1100

    Implement a ENV Var for func test protocol

--- functional_tests.py
+++ functional_tests.py
@@ -5,12 +5,13 @@ from selenium.webdriver import FirefoxOptions
 
 host = os.getenv("DJANGO_EXTERNAL_HOST_NAME", "127.0.0.1")
 port = os.getenv("DJANGO_EXTERNAL_PORT_NUM", "80")
+protocol = os.getenv("DJANGO_EXTERNAL_PROTOCOL", "http")
 
 opts = FirefoxOptions()
 opts.add_argument("--headless")
 
 browser = webdriver.Firefox(options=opts)
-browser.get(f'http://{host}:{port}')
+browser.get(f'{protocol}://{host}:{port}')
 
 
 def test_deployment_successful():
