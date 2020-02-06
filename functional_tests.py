8881e96116a267d3d276860ce3624f337241ee52
Ben Elliot <benjiboi214@gmail.com>
2020-02-06 21:46:07 +1100

    Change defaults

--- functional_tests.py
+++ functional_tests.py
@@ -3,8 +3,8 @@ import os
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 
-host = os.getenv("DJANGO_EXTERNAL_HOST_NAME", "")
-port = os.getenv("DJANGO_EXTERNAL_PORT_NUM", "")
+host = os.getenv("DJANGO_EXTERNAL_HOST_NAME", "127.0.0.1")
+port = os.getenv("DJANGO_EXTERNAL_PORT_NUM", "80")
 
 opts = FirefoxOptions()
 opts.add_argument("--headless")
