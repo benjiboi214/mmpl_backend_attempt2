3c9cfaac09381d9f90d1459c1d78cfb2baf7dfb0
Ben Elliot <benjiboi214@gmail.com>
2020-02-09 15:53:33 +1100

    Revert more to the current working dev branch

--- docker/test/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o pipefail
-set -o nounset
-
-pipenv run python manage.py makemigrations
-pipenv run python manage.py migrate
-pipenv run python manage.py runserver_plus 0.0.0.0:$DJANGO_INTERNAL_PORT_NUM
