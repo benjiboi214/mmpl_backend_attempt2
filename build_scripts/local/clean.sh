aabf19ba86661e572635dafb41db4d00b69f0857
Ben Elliot <benjiboi214@gmail.com>
2020-02-09 16:58:48 +1100

    Feature/postgresql support (#16)
    * Add postgres, start sorting out project structure and image structure
    
    * Revert build script for now.
    
    * Revert more to the current working dev branch
    
    * Move config back out to parent dir. Not ready to move yet.
    
    * Update the env example with postgres params
    
    * Ignore the new separation between envs
    
    * Add new build script for running everything end to end
    
    * Ensure psycopg2 is installed on image
    
    * Update to point to new env location locally

--- build_scripts/local/clean.sh
+++ build_scripts/local/clean.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-. .env
+. .env.test
 
 ## Get rid of dead containers ##
 docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs docker rm || true
 
 ## Clean up dangling images ##
-docker rmi -f $(docker images -f "dangling=true" -q) || trues
+docker rmi -f $(docker images -f "dangling=true" -q) || true
 
 ## Clean up testing reports
 rm $PYTEST_UNIT_REPORT_PATH
