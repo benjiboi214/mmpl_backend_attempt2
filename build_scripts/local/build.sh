d6ad1f18467e350235d0d49f0335ff7f7402a792
Ben Elliot <benjiboi214@gmail.com>
2020-02-09 15:47:30 +1100

    Revert build script for now.

--- build_scripts/local/build.sh
+++ build_scripts/local/build.sh
@@ -1,15 +1,3 @@
 #/bin/bash
 
-#!/bin/bash
-
-if [ "$1" == "" ]; then
-    echo "Error: Unsupported command. Select from \"test\", \"prod\""
-elif [ "$1" == "test" ]; then
-    docker build -f docker/test/Dockerfile .
-elif [ "$1" == "prod" ]; then
-    docker build -f docker/prod/Dockerfile .
-else
-    echo "Error: Unsupported command. Select from XXX"
-fi
-
-
+docker build .
