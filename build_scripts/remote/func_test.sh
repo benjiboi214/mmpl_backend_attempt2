4c0ff26d4442eff8e23d2c8e2be60a7ba4357862
Ben Elliot <benjiboi214@gmail.com>
2020-02-09 15:06:29 +1100

    No longer need to specify testing env for testing run

--- build_scripts/remote/func_test.sh
+++ build_scripts/remote/func_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DJANGO_SETTINGS_MODULE="configuration.api.settings.test"
-
 echo "#### Running PyTest ####"
 pipenv run pytest functional_tests.py \
   -c $PYTEST_FUNC_CONFIG_PATH \
--- build_scripts/remote/unit_test.sh
+++ build_scripts/remote/unit_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DJANGO_SETTINGS_MODULE="configuration.api.settings.test"
-
 echo "#### Running PyTest ####"
 pipenv run pytest ./api \
   -c $PYTEST_UNIT_CONFIG_PATH \
