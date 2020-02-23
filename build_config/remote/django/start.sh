92fb96b47fef88945e23a1146da0b6d59ebd305e
Ben Elliot <benjiboi214@gmail.com>
2020-02-23 16:20:08 +1100

    Remove collectstatic until we have implemented properly

--- build_config/remote/django/start.sh
+++ build_config/remote/django/start.sh
@@ -4,9 +4,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-pipenv run python /app/manage.py collectstatic --noinput
+# pipenv run python /app/manage.py collectstatic --noinput
 pipenv run /usr/local/bin/gunicorn config.wsgi --bind 0.0.0.0:$DJANGO_INTERNAL_PORT_NUM
-
-pipenv run python manage.py makemigrations
-pipenv run python manage.py migrate
-pipenv run python manage.py runserver_plus 0.0.0.0:$DJANGO_INTERNAL_PORT_NUM
