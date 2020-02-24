8bbe28f17f7d47ccfb8fafd66b980ad6b86368f5
Ben Elliot <benjiboi214@gmail.com>
2020-02-25 07:50:44 +1100

    Add the collect static command to push the static files

--- build_config/remote/django/start.sh
+++ build_config/remote/django/start.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# pipenv run python /app/manage.py collectstatic --noinput
+pipenv run python /app/manage.py collectstatic --noinput
 pipenv run gunicorn configuration.api.wsgi --bind 0.0.0.0:$DJANGO_INTERNAL_PORT_NUM
