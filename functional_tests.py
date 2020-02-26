8655bcd591892336dd1f9ee3d673d5b55fdcf626
Ben Elliot <benjiboi214@gmail.com>
2020-02-26 23:06:38 +1100

    Add functional tests for new templates

--- functional_tests.py
+++ functional_tests.py
@@ -1,18 +1,39 @@
 import os
 
+
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
-
-host = os.getenv("DJANGO_EXTERNAL_HOST_NAME", "127.0.0.1")
-port = os.getenv("DJANGO_EXTERNAL_PORT_NUM", "80")
-protocol = os.getenv("DJANGO_EXTERNAL_PROTOCOL", "http")
-
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-
-browser = webdriver.Firefox(options=opts)
-browser.get(f'{protocol}://{host}:{port}')
+import unittest
 
 
-def test_deployment_successful():
-    assert 'Under Construction' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        opts = FirefoxOptions()
+        opts.add_argument("--headless")
+        self.browser = webdriver.Firefox(options=opts)
+        self.host = os.getenv("DJANGO_EXTERNAL_HOST_NAME", "127.0.0.1")
+        self.port = os.getenv("DJANGO_EXTERNAL_PORT_NUM", "80")
+        self.protocol = os.getenv("DJANGO_EXTERNAL_PROTOCOL", "http")
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def _get_host_url(self):
+        return f'{self.protocol}://{self.host}:{self.port}'
+
+    def test_home_page_is_under_construction(self):
+        self.browser.get(self._get_host_url())
+
+        # Ensure Under Construction is in the title
+        self.assertIn('Under Construction', self.browser.title)
+
+    def test_404_page_is_served(self):
+        self.browser.get(self._get_host_url() + "/this-does-not-exst")
+
+        # Ensure Under Construction is in the title
+        self.assertIn('Page not found', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
