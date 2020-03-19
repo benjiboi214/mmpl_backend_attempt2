4c0e5f1429400d2a98979320fdd38dfd399c0bc2
Ben Elliot <benjiboi214@gmail.com>
2020-03-20 10:29:39 +1100

    Removed DB URL requirement

--- configuration/api/settings/base.py
+++ configuration/api/settings/base.py
@@ -61,9 +61,7 @@ USE_TZ = True
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {
-    'default': env.db('DATABASE_URL'),
-}
+DATABASES = {}
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
 
