e1ae9792d7283fdae8dedc19b99520283004b09d
Ben Elliot <benjiboi214@gmail.com>
2020-03-19 15:43:18 +1100

    Clean up flake formatting

--- build_config/pipeline/test_traffic/function/src/lambda_function.py
+++ build_config/pipeline/test_traffic/function/src/lambda_function.py
@@ -1,32 +1,24 @@
 import os
-import pprint
 
-import importlib_metadata
-
-from selenium.webdriver.common.keys import Keys
 import boto3
-
-
 import pytest
-from functional_tests import NewVisitorTest
 
 client = boto3.client('codedeploy')
 
 
 def lambda_handler(event, context, *args, **kwargs):
     # Get and print the environment for debugging.
-    env_var = os.environ
     os.environ['PYTEST_LAMBDA_FLAG'] = "True"
 
     exit_code = pytest.main([
-        "-s", # Disable capturing of syslog for debugging
-        "-p", "no:cacheprovider", # Don't try to write pycache files
+        "-s",  # Disable capturing of syslog for debugging
+        "-p", "no:cacheprovider",  # Don't try to write pycache files
         "/var/task/src/functional_tests.py"
     ], plugins=[])
 
     if exit_code == pytest.ExitCode.OK:
         print("=== Test Condition PASSED ===")
-        response = client.put_lifecycle_event_hook_execution_status(
+        client.put_lifecycle_event_hook_execution_status(
             deploymentId=event['DeploymentId'],
             lifecycleEventHookExecutionId=event['LifecycleEventHookExecutionId'],
             status='Succeeded'
@@ -34,7 +26,7 @@ def lambda_handler(event, context, *args, **kwargs):
         return
     else:
         print("=== Test Condition FAILED ===")
-        response = client.put_lifecycle_event_hook_execution_status(
+        client.put_lifecycle_event_hook_execution_status(
             deploymentId=event['DeploymentId'],
             lifecycleEventHookExecutionId=event['LifecycleEventHookExecutionId'],
             status='Failed'
