b6c33c2c35f29ee8d083d5699fc16c94cf337320
Ben Elliot <benjiboi214@gmail.com>
2020-03-20 16:52:33 +1100

    Fix syntax

--- build_scripts/pipeline/publish.sh
+++ build_scripts/pipeline/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sam deploy \
-    --debug
+    --debug \
     --s3-bucket systemiphus-lambda-artifact-storage \
     --capabilities  CAPABILITY_NAMED_IAM \
     --stack-name mmpl-backend
