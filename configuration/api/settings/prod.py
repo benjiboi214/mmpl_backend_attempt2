5b1c139214422deaf258984a4c229217a7b20a2a
Ben Elliot <benjiboi214@gmail.com>
2020-03-22 01:47:31 +1100

    Add mailgun config items

--- configuration/api/settings/prod.py
+++ configuration/api/settings/prod.py
@@ -139,35 +139,35 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [  # noqa F405
 ]
 
 
-# EMAIL
-# # ------------------------------------------------------------------------------
-# # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
-# DEFAULT_FROM_EMAIL = env(
-#     'DJANGO_DEFAULT_FROM_EMAIL',
-#     default='mmpl_backend <noreply@mmpl.org.au>'
-# )
-# # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
-# SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
-# # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
-# EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[mmpl_backend]')
-
-
 # ADMIN
 # ------------------------------------------------------------------------------
 # Django Admin URL regex.
 ADMIN_URL = env('DJANGO_ADMIN_URL')
 
 
-# # Anymail (Mailgun)
-# # ------------------------------------------------------------------------------
-# # https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
-# INSTALLED_APPS += ['anymail']  # noqa F405
-# EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
-# # https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
-# ANYMAIL = {
-#     'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
-#     'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN')
-# }
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
+DEFAULT_FROM_EMAIL = env(
+    'DJANGO_DEFAULT_FROM_EMAIL',
+    default='MMPL Staging <mmpl_staging@systemiphus.com>'
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#server-email
+SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
+EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[MMPL Staging]')
+
+
+# Anymail (Mailgun)
+# ------------------------------------------------------------------------------
+# https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
+INSTALLED_APPS += ['anymail']  # noqa F405
+EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
+# https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
+ANYMAIL = {
+    'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
+    'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN')
+}
 
 
 # Gunicorn
