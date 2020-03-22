f8323b000b9049f3b94ac06281fa61d6b6309902
Ben Elliot <benjiboi214@gmail.com>
2020-03-22 20:17:11 +1100

    Reduce timeout

--- build_config/pipeline/test_traffic/function/src/lambda_function.py
+++ build_config/pipeline/test_traffic/function/src/lambda_function.py
@@ -8,7 +8,7 @@ client = boto3.client('codedeploy')
 
 
 def lambda_handler(event, context, *args, **kwargs):
-    time.sleep(250)
+    time.sleep(20)
 
     # Get and print the environment for debugging.
     os.environ['PYTEST_LAMBDA_FLAG'] = "True"
