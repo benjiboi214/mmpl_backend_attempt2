d6e706553c8358e02d930aa6649a7997c914714f
Ben Elliot <benjiboi214@gmail.com>
2020-03-22 21:04:52 +1100

    Add logging options to gunicorn

--- build_config/remote/django/start.sh
+++ build_config/remote/django/start.sh
@@ -5,4 +5,6 @@ set -o pipefail
 set -o nounset
 
 pipenv run python /app/manage.py collectstatic --noinput
-pipenv run gunicorn configuration.api.wsgi --bind 0.0.0.0:$DJANGO_INTERNAL_PORT_NUM
+pipenv run gunicorn configuration.api.wsgi \
+    --access-logfile '-' --error-logfile '-' \
+    --bind 0.0.0.0:$DJANGO_INTERNAL_PORT_NUM
