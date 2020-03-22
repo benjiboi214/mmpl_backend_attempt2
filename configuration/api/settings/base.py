0267d94302c3a55e610a11fd03e291815cf631e4
Ben Elliot <benjiboi214@gmail.com>
2020-03-22 22:07:22 +1100

    Try fixing commas?

--- configuration/api/settings/base.py
+++ configuration/api/settings/base.py
@@ -261,7 +261,7 @@ EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.s
 ADMIN_URL = 'admin/'
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = [
-    ("""Ben Elliot""", 'benjiboi214@gmail.com'),
+    ('Ben Elliot', 'benjiboi214@gmail.com'),
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
