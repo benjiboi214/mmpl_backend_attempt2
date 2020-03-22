28ac7630f28543aef6ad2ee0027a08d56cba057e
Ben Elliot <benjiboi214@gmail.com>
2020-03-22 21:06:45 +1100

    Up log level to debug

--- build_config/remote/django/start.sh
+++ build_config/remote/django/start.sh
@@ -6,5 +6,6 @@ set -o nounset
 
 pipenv run python /app/manage.py collectstatic --noinput
 pipenv run gunicorn configuration.api.wsgi \
+    --log-level debug \
     --access-logfile '-' --error-logfile '-' \
     --bind 0.0.0.0:$DJANGO_INTERNAL_PORT_NUM
