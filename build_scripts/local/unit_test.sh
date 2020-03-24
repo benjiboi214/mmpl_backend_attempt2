fe150b1c6260524c2b3aec36368bd403654bf3e6
Ben Elliot <benjiboi214@gmail.com>
2020-03-24 18:33:39 +1100

    Add missing lines to coverage report

--- build_scripts/local/unit_test.sh
+++ build_scripts/local/unit_test.sh
@@ -6,7 +6,7 @@ echo "#### Running PyTest ####"
 pipenv run pytest ./api \
   -c $PYTEST_UNIT_CONFIG_PATH \
   --cov=api \
-  --cov-report term
+  --cov-report term-missing
 
 echo "#### Running Flake8 ####"
 pipenv run flake8 \
