4ce414097536f57fefbab16998be97f784538279
Ben Elliot <benjiboi214@gmail.com>
2020-03-25 16:34:37 +1100

    Fix flake formatting

--- api/contrib/tests.py
+++ api/contrib/tests.py
@@ -8,7 +8,7 @@ class TestSitesMigrations(TransactionTestCase):
     def test_default_site_updated(self):
         migrator = Migrator(database='default')
         migrator.before(('sites', '0002_alter_domain_unique'))
-        
+
         new_state = migrator.after(('sites', '0003_set_site_domain_and_name'))
         Site = new_state.apps.get_model('sites', 'Site')
         site = Site.objects.get(id=settings.SITE_ID)
@@ -21,7 +21,7 @@ class TestSitesMigrations(TransactionTestCase):
     def test_default_site_rollback(self):
         migrator = Migrator(database='default')
         migrator.before(('sites', '0003_set_site_domain_and_name'))
-        
+
         new_state = migrator.after(('sites', '0002_alter_domain_unique'))
         Site = new_state.apps.get_model('sites', 'Site')
         site = Site.objects.get(id=settings.SITE_ID)
