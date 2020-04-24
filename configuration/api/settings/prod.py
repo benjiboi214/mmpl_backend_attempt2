8e2f3182ff081524403454961266662c8db4c28d
Ben Elliot <benjiboi214@gmail.com>
2020-04-24 20:04:18 +1000

    Fix assumption in sentry init

--- configuration/api/settings/prod.py
+++ configuration/api/settings/prod.py
@@ -248,10 +248,13 @@ SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
 sentry_logging = LoggingIntegration(
     level=SENTRY_LOG_LEVEL,  # Capture info and above as breadcrumbs
     event_level=logging.ERROR,  # Send errors as events
-    send_default_pii=True
 )
 
-sentry_sdk.init(dsn=SENTRY_DSN, integrations=[sentry_logging, DjangoIntegration()])
+sentry_sdk.init(
+    dsn=SENTRY_DSN, 
+    integrations=[sentry_logging, DjangoIntegration()],
+    send_default_pii=True
+)
 
 # Custom
 # ------------------------------------------------------------------------------
